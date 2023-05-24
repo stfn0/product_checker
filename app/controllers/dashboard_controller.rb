da7c1141b4bd4f2873121c4849a5b2a49b178cbb
stfn0 <stefanojp@gmail.com>
2023-05-23 21:31:43 -0300

    feat: validates url

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,47 +1,51 @@
 # app/controllers/dashboard_controller.rb
 require 'uri'
+require 'net/http'
 
 class DashboardController < ApplicationController
-  # ...
-  include ProductHelper  
-  # def process_url(url)
-  #   puts "###################################"
-  #   puts url
-  #   puts "###################################"
-  #   product = create_product(url)
-  # end
+  include ProductHelper 
 
   def add_product
-
     url = params[:url]
-    has_product = Current.user.products.where(url: params[:url]).first
-    
-    if (has_product.nil?)
-      if (is_json_url?(url))
-        product = create_product(url)      
-        Current.user.products << product 
 
-        redirect_to dashboard_path, notice: "Product added successfully."
-      else
-        return wrong_url()
-      end
-    else
-      redirect_to dashboard_path, alert: "You already have the URL added."
+    #has_product pra não adicionar o msm URL caso já exista
+    has_product = Current.user.products.where(url: params[:url]).first
+
+    if !(has_product.nil?)
+      redirect_to dashboard_path, alert: "You already have the URL added." 
+      return     
     end
+
+    if !(is_valid_url(url))
+      redirect_to dashboard_path, alert: "Invalid product URL."
+      return
+    end
+               
+    response = get_url_data(url)
+
+    if(response && is_response_valid(response))
+      product = create_product(url)      
+      Current.user.products << product 
+      redirect_to dashboard_path, notice: "Product added successfully."   
+    else
+      redirect_to dashboard_path, alert: "Couldn't add the product, check your URL."
+    end       
   end 
   
-
-  def wrong_url()
-    redirect_to dashboard_path, alert: "Invalid product URL."
+  def is_response_valid(response)                
+    return response.is_a?(Net::HTTPSuccess) && response.content_type == 'application/json'     
   end
-end
-
-
-def is_json_url?(url)
-  uri = URI.parse(url)
-  return false unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
   
-  # Check if the path or extension corresponds to JSON format
-  return true if uri.path.end_with?('.json') || File.extname(uri.path) == '.json'
-  false
+  def is_valid_url(url)    
+    return url =~ /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+  end
+
+  def get_url_data(url)
+    begin    
+      uri = URI.parse(url)  
+      return Net::HTTP.get_response(uri)       
+    rescue URI::InvalidURIError, SocketError, Errno::ECONNREFUSED
+      return nil  # Handle invalid or unreachable URLs by returning false
+    end
+  end
 end
